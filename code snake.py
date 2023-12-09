f62bcd7c3c0d9e235f712a39363ac1bc374238b3
Marc Samba <marc.samba@etu.minesparis.psl.eu>
2023-12-09 16:00:14 +0100

    checkerboard

--- code snake.py
+++ code snake.py
@@ -3,10 +3,16 @@ import pygame
 #initialisation des modules
 pygame.init() 
 #constantes
+
 WIDTH=400
 HEIGHT=300
 SCREEN_COLOR=(0,0,0)
+WHITE=(255,255,255)
 FPS=20
+TILE_SIZE=20
+LINE=HEIGHT/TILE_SIZE
+COLUMN=WIDTH/TILE_SIZE
+
 #création de l'écran
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
@@ -22,7 +28,13 @@ while running:
                 running=False
         elif event.type==pygame.QUIT:
             running=False
-
+    
+    #dessin du checkboard
+    for i in range(LINE):
+        for j in range(COLUMN):
+            if (i+j)%2==0:
+                new_rect=pygame.Rect((j*TILE_SIZE,i*TILE_SIZE,TILE_SIZE,TILE_SIZE))
+                pygame.draw.rect(screen,WHITE,new_rect)
     
     screen.fill( SCREEN_COLOR ) #affichage de l'écran
 
